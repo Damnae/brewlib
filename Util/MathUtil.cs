847ee063c2467068b74d97d2ea8a2ae38e803c9d
Damnae <damnaee@gmail.com>
2020-08-07 17:47:00 +0200

    Add missing math functions from hydra.

--- Util/MathUtil.cs
+++ Util/MathUtil.cs
@@ -9,5 +9,22 @@ namespace BrewLib.Util
 
         public static bool DoubleEquals(double a, double b, double epsilon)
             => Math.Abs(a - b) < epsilon;
+
+        public static int NextPowerOfTwo(int v)
+        {
+            v--;
+            v |= v >> 1;
+            v |= v >> 2;
+            v |= v >> 4;
+            v |= v >> 8;
+            v |= v >> 16;
+            return v + 1;
+        }
+
+        public static double ShortestAngleDelta(double from, double to)
+        {
+            var rotationDelta = to - from;
+            return rotationDelta - (Math.Floor((rotationDelta + Math.PI) / (Math.PI * 2)) * (Math.PI * 2));
+        }
     }
 }
