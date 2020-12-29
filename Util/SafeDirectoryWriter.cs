a0a17ce5ee0e0e1ae987b8029f3206d32743963a
Damnae <damnaee@gmail.com>
2020-12-29 12:47:33 +0100

    Add file checks to SafeDirectoryWriter.

--- Util/SafeDirectoryWriter.cs
+++ Util/SafeDirectoryWriter.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace BrewLib.Util
 {
     public class SafeDirectoryWriter : IDisposable
     {
+        private readonly List<string> paths = new List<string>();
         private readonly string targetDirectory;
         private readonly string tempDirectory;
         private readonly string backupDirectory;
@@ -23,10 +25,33 @@ namespace BrewLib.Util
         }
 
         public string GetPath(string path)
-            => Path.Combine(tempDirectory, path);
+        {
+            var fullpath = Path.Combine(tempDirectory, path);
+            paths.Add(fullpath);
+            return fullpath;
+        }
 
-        public void Commit()
-            => committed = true;
+        public void Commit(bool checkPaths = true)
+        {
+            if (checkPaths)
+            {
+                if (paths.Count == 0)
+                    throw new InvalidOperationException($"No file path requested");
+
+                foreach (var path in paths)
+                {
+                    var file = new FileInfo(path);
+
+                    if (!file.Exists)
+                        throw new InvalidOperationException($"File path requested but not created: {path}");
+
+                    if (file.Length == 0)
+                        throw new InvalidOperationException($"File path requested but is empty: {path}");
+                }
+            }
+
+            committed = true;
+        }
 
         public void Dispose()
         {
